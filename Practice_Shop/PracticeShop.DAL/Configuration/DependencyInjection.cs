b4509ab22bc2090b9a6b8910ff6d076c2a56e189
Nikita Kyteyko <nikitakuteyko@gmail.com>
2024-06-25 16:11:40 +0300

    Setting up project structure

---
+++ Practice_Shop/PracticeShop.DAL/Configuration/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeShop.DAL.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDAL(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
