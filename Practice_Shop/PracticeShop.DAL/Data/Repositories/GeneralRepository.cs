9a47726e6edac4197c470fc8a9d09ff99f0aff89
Nikita Kyteyko <nikitakuteyko@gmail.com>
2024-06-27 16:29:10 +0300

    Implement general repository

---
+++ Practice_Shop/PracticeShop.DAL/Data/Repositories/GeneralRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeShop.DAL.Data.Repositories.Interfaces;
+using PracticeShop.WebAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeShop.DAL.Data.Repositories
+{
+    public class GeneralRepository<T> : IGeneralRepository<T> where T : class
+    {
+        public ApplicationContext dbContext;
+        public DbSet<T> table;
+        public GeneralRepository(ApplicationContext dbContext)
+        {
+            this.dbContext = dbContext;
+            table = this.dbContext.Set<T>();
+        }
+        public async Task Add(T entity)
+        {
+            await table.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            table.Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await table.ToListAsync<T>();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await table.FindAsync(id);
+        }
+
+        public async Task Update(T entity)
+        {
+            table.Update(entity);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
