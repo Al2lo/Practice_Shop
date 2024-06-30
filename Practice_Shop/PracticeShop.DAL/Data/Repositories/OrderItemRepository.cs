6db1133f43343f3042ccdd80c49a09429ee9c124
Alexandr_Lomako <grod_as@mail.ru>
2024-06-30 17:27:09 +0300

    first commit

---
+++ Practice_Shop/PracticeShop.DAL/Data/Repositories/OrderItemRepository.cs
@@ -0,0 +1,11 @@
+﻿using PracticeShop.DAL.Entities;
+
+namespace PracticeShop.DAL.Data.Repositories
+{
+    public class OrderItemRepository:GeneralRepository<OrderItem>
+    {
+        public OrderItemRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
