910d931163c7f338d9ca42e6cf14965dbf3737f0
Lexa <aadamovich2003@gmail.com>
2024-07-01 01:13:53 +0300

    implement product repository logic

---
+++ Practice_Shop/PracticeShop.DAL/Data/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using PracticeShop.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeShop.DAL.Data.Repositories.Interfaces
+{
+    internal interface IProductRepository<T>
+    {
+        public Task<IEnumerable<T>> GetByCategory(Category category);
+    }
+}
---
+++ Practice_Shop/PracticeShop.DAL/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeShop.DAL.Data.Repositories.Interfaces;
+using PracticeShop.DAL.Entities;
+using PracticeShop.WebAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeShop.DAL.Data.Repositories
+{
+    internal class ProductRepository : GeneralRepository<Product>, IProductRepository<Product>
+    {
+        public ProductRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Task<IEnumerable<Product>> GetByCategory(Category category)
+        {
+            return (Task<IEnumerable<Product>>)table.Where<Product>(prod => prod.Category.Id == category.Id);
+        }
+    }
+}
