5e4f7168897d65c255187855f63588244d5841c3
Alexandr_Lomako <grod_as@mail.ru>
2024-06-30 20:20:34 +0300

    add interface IOrderItemRepository

--- Practice_Shop/PracticeShop.DAL/Data/Repositories/OrderItemRepository.cs
+++ Practice_Shop/PracticeShop.DAL/Data/Repositories/OrderItemRepository.cs
@@ -1,8 +1,9 @@
-﻿using PracticeShop.DAL.Entities;
+﻿using PracticeShop.DAL.Data.Repositories.Interfaces;
+using PracticeShop.DAL.Entities;
 
 namespace PracticeShop.DAL.Data.Repositories
 {
-    public class OrderItemRepository:GeneralRepository<OrderItem>
+    public class OrderItemRepository:GeneralRepository<OrderItem>, IOrderItemRepository
     {
         public OrderItemRepository(ApplicationContext dbContext) : base(dbContext)
         {
