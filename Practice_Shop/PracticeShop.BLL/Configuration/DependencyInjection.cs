c2ab4a2e9e847ae600e6cc194f51cc62fc7fbf43
Lexa <aadamovich2003@gmail.com>
2024-07-01 10:48:14 +0300

    fix di

--- Practice_Shop/PracticeShop.BLL/Configuration/DependencyInjection.cs
+++ Practice_Shop/PracticeShop.BLL/Configuration/DependencyInjection.cs
@@ -1,7 +1,9 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using PracticeShop.BLL.DTOs;
+using PracticeShop.BLL.DTOs.Product;
 using PracticeShop.BLL.Validation;
+using PracticeShop.BLL.Validation.Product;
 
 namespace PracticeShop.BLL.Configuration
 {
@@ -11,6 +13,7 @@ namespace PracticeShop.BLL.Configuration
         {
             services.AddScoped<IValidator<OrderItemDTO>, OrderItemDTOValidator>();
             services.AddScoped<IValidator<ProductDTO>, ProductDTOValidator>();
+            services.AddScoped<IValidator<UpdateProductDTO>, UpdateProductDTOValidator>();
 
             return services;
         }
