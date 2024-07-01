5c0d3cd76b67a9deb902d4b26b55ca7af7438f11
ilya <ilya@mail.ru>
2024-07-01 11:05:58 +0300

    add methods delete and get users

--- Practice_Shop/PracticeShop.BLL/Services/Interfaces/IUserService.cs
+++ Practice_Shop/PracticeShop.BLL/Services/Interfaces/IUserService.cs
@@ -5,7 +5,9 @@ namespace PracticeShop.BLL.Services.Interfaces
 {
     public interface IUserService
     {
+        Task<IEnumerable<UserDTO>> GetAllAsync();
         Task CreateUserAsync(UserDTO user);
         Task UpdateUserAsync(UpdateUser user);
+        Task DeleteUserAsync(Guid id);
     }
 }
