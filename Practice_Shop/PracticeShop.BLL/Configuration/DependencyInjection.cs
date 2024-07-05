6f7910a16e2ef63a552b497d3500860f5afaa3f2
Lomako_Aliaksandr <95307204+Al2lo@users.noreply.github.com>
2024-07-05 12:48:56 +0300

    Update DependencyInjection.cs

--- Practice_Shop/PracticeShop.BLL/Configuration/DependencyInjection.cs
+++ Practice_Shop/PracticeShop.BLL/Configuration/DependencyInjection.cs
@@ -2,10 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using PracticeShop.BLL.DTOs.Category;
 using PracticeShop.BLL.Validation.Category;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using PracticeShop.BLL.DTOs;
 using PracticeShop.BLL.DTOs.Product;
